62e1b69bd7325b21f3d78aeb3595ea9e6c256df9
TheJazMaster <aljazpero@hotmail.com>
2024-01-07 13:59:08 +0100

    Fixes & balance tweaks

---
+++ Actions/AAddCardAdjusted.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eddie.Actions
+{
+    public class AAddCardAdjusted : AAddCard
+    {
+        public int amountAdjustment = 0;
+        public int amountDisplayAdjustment = 0;
+
+        public override void Begin(G g, State s, Combat c)
+	    {
+            var oldAmount = amount;
+            amount = Math.Max(0, amount + amountAdjustment);
+            base.Begin(g, s, c);
+            amount = oldAmount;
+        }
+        public override Icon? GetIcon(State s)
+        {
+            var oldAmount = amount;
+            amount = Math.Max(0, amount + amountDisplayAdjustment);
+            Icon? icon = base.GetIcon(s);
+            amount = oldAmount;
+            return icon;   
+        }
+    }
+}
---
+++ Cards/Surge.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eddie.Cards;
+
+[CardMeta(rarity = Rarity.common, upgradesTo = new Upgrade[] { Upgrade.A, Upgrade.B }, dontOffer = true)]
+public class Surge : Card
+{
+    private static readonly Lazy<Spr> art = new(() => Enum.Parse<Spr>("cards_Overdrive"));
+
+    public override string Name() => "Surge";
+
+    public override CardData GetData(State state)
+    {
+        return new CardData()
+        {
+            cost = 1,
+            exhaust = true,
+            temporary = true,
+            art = art.Value
+        };
+    }
+
+    public override List<CardAction> GetActions(State s, Combat c)
+    {
+        switch (upgrade)
+        {
+            case Upgrade.None:
+                return new List<CardAction>
+                {
+                    new AAttack {
+                        damage = GetDmg(s, 2)
+                    },
+                    new AStatus {
+                        status = Status.overdrive,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+            case Upgrade.A:
+                return new List<CardAction>
+                {
+                    new AAttack {
+                        damage = GetDmg(s, 3)
+                    },
+                    new AStatus {
+                        status = Status.overdrive,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+            case Upgrade.B:
+                return new List<CardAction>
+                {
+                    new AAttack {
+                        damage = GetDmg(s, 1),
+                        stunEnemy = true
+                    },
+                    new AStatus {
+                        status = Status.overdrive,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    }
+                };
+            default:
+                return new List<CardAction>();
+        }
+    }
+}
