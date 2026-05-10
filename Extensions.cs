23990bf2079363f32161722d7f0b4224533cc93d
Aljaž Perovšek <aljazpero@hotmail.com>
2026-05-10 18:27:39 +0200

    Code cleanup

--- Extensions.cs
+++ Extensions.cs
@@ -1,105 +1,22 @@
+using System.Reflection;
 using HarmonyLib;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
 
 namespace TheJazMaster.Eddie;
 
-internal static class Extensions
-{
-	static int recursionLevel = 0;
-
-	public static int GetCurrentCostNoRecursion(this Card card, State s)
-	{
-		int result;
-		recursionLevel++;
-		if (recursionLevel < 2)
-			result = card.GetCurrentCost(s);
-		else
-			result = card.GetData(s).cost;
-		recursionLevel = 0;
-		return result;
-	}
-
-	public static void QueueImmediate<T>(this List<T> source, T item)
-	{
-		source.Insert(0, item);
-	}
-
-	public static SaySwitch MaybeAdd(this SaySwitch source, bool condition, CustomSay item)
-	{
-		if (condition) source.lines.Add(item);
-		return source;
-	}
-
-	public static void Queue<T>(this List<T> source, T item)
-	{
-		source.Add(item);
-	}
-
-	private static void WarnOnDebugAssembly(ILogger logger, Assembly? assembly)
-	{
-		if (assembly?.IsBuiltInDebugConfiguration() == true)
-			logger.LogWarning("{Assembly} was built in debug configuration - patching may fail. If it does fail, please ask that mod's developer to build it in release configuration.", assembly.GetName().Name);
-	}
-
-	public static T GetModulo<T>(this T[] arr, int index)
-	{
-		return arr[Mutil.Mod(index, arr.Length)];
-	}
-
-	public static T Random<T>(this List<T> list, Rand rng)
-	{
-		return list[rng.NextInt() % list.Count];
-	}
-
-	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
-	{
-		Random rnd = new Random();
-		return source.OrderBy((T item) => rnd.Next());
-	}
-
-	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Rand rng)
-	{
-		Rand rng2 = rng;
-		return source.OrderBy((T item) => rng2.NextInt());
-	}
-
-	public static bool TryPatch(
-		this Harmony self,
-		MethodInfo? original,
-		ILogger logger,
-		LogLevel problemLogLevel = LogLevel.Error,
-		LogLevel successLogLevel = LogLevel.Trace,
-		HarmonyMethod? prefix = null,
-		HarmonyMethod? postfix = null,
-		HarmonyMethod? transpiler = null,
-		HarmonyMethod? finalizer = null
-	)
-	{
-		var originalMethod = original;
-		if (originalMethod == null)
-		{
-			logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: Unknown method to patch.");
-			return false;
+public static class Extensions {
+    public static MethodInfo? TryPatch(this Harmony harmony, ILogger logger, MethodBase original, HarmonyMethod? prefix = null, HarmonyMethod? postfix = null, HarmonyMethod? transpiler = null, HarmonyMethod? finalizer = null) {
+		try {
+			return harmony.Patch(original, prefix, postfix, transpiler, finalizer);
+		} catch (Exception e) {
+			logger.LogError(original.Name + " patch has failed. Reason:\n" +
+				e.Message + "\n" + e.StackTrace);
+			return null;
 		}
+	}
 
-		try
-		{
-
-			if (transpiler is not null)
-				WarnOnDebugAssembly(logger, originalMethod.DeclaringType?.Assembly);
-			self.Patch(originalMethod, prefix, postfix, transpiler, finalizer);
-			logger.Log(successLogLevel, "Patched method {Method}.", originalMethod.FullDescription());
-			return true;
-		}
-		catch (Exception ex)
-		{
-			logger.Log(problemLogLevel, "Could not patch method {Method} - the mod may not work correctly.\nReason: {Exception}", originalMethod, ex);
-			return false;
-		}
+	public static T WithModData<T, K>(this T thing, string key, K data) {
+		ModEntry.Instance.Helper.ModData.SetModData(thing!, key, data);
+		return thing;
 	}
 }
