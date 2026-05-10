297135a8618b83e28230bc330bd685ae839d78ce
Aljaž Perovšek <aljazpero@hotmail.com>
2026-05-10 18:24:25 +0200

    Doubled artifact effect and made it trigger before overheat

--- Artifacts/Duo/EmergencyVentilator.cs
+++ Artifacts/Duo/EmergencyVentilator.cs
@@ -1,20 +1,36 @@
 
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
 namespace TheJazMaster.Eddie.Artifacts;
 
-[ArtifactMeta(pools = [ArtifactPool.Common])]
-public class EmergencyVentilator : Artifact
+public class EmergencyVentilator : Artifact, IRegisterableArtifact
 {
+    public static void Register(Deck deck, IModHelper helper, IPluginPackage<IModManifest> package)
+    {
+		if (ModEntry.Instance.DuoArtifactsApi != null) {
+			IRegisterableArtifact.Register(
+				MethodBase.GetCurrentMethod()!.DeclaringType!,
+				ModEntry.Instance.DuoArtifactsApi.DuoArtifactVanillaDeck,
+				[ArtifactPool.Common],
+				helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile($"Sprites/artifact_icons/duos/emergency_ventilator.png")).Sprite
+			);
+			ModEntry.Instance.DuoArtifactsApi.RegisterDuoArtifact(MethodBase.GetCurrentMethod()!.DeclaringType!, [ModEntry.Instance.EddieDeck, Deck.eunice]);
+		}
+    }
+	
 	public override void OnTurnEnd(State state, Combat combat)
 	{
 		if (combat.energy > 0)
 		{
-			combat.QueueImmediate(new AStatus
-			{
+			new AStatus {
 				status = Status.heat,
-				statusAmount = -1,
+				statusAmount = -2,
 				targetPlayer = true,
 				artifactPulse = Key()
-			});
+			}.Begin(MG.inst.g, state, combat);
+			Pulse();
 		}
 	}
 
