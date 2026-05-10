4680a8c5e94eec0a6bd3ecdb2748353276aa06b6
Aljaž Perovšek <aljazpero@hotmail.com>
2026-05-10 18:25:22 +0200

    Nerfed

--- Artifacts/Duo/WaxWings.cs
+++ Artifacts/Duo/WaxWings.cs
@@ -1,35 +1,46 @@
-using TheJazMaster.Eddie.Cards;
-using System.Collections.Generic;
+
 using System.Reflection;
-using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Soggins;
 
 namespace TheJazMaster.Eddie.Artifacts;
 
-[ArtifactMeta(pools = [ArtifactPool.Common])]
-public class WaxWings : Artifact, ISmugHook
+public class WaxWings : Artifact, ISmugHook, IRegisterableArtifact
 {
-	static ISogginsApi? SogginsAPI => Manifest.Instance.SogginsApi;
+	static ISogginsApi? SogginsAPI => ModEntry.Instance.SogginsApi;
 
 	public bool active = false;
 
+	public static void Register(Deck deck, IModHelper helper, IPluginPackage<IModManifest> package)
+	{
+		if (ModEntry.Instance.DuoArtifactsApi != null && SogginsAPI != null) {
+			IRegisterableArtifact.Register(
+				MethodBase.GetCurrentMethod()!.DeclaringType!,
+				ModEntry.Instance.DuoArtifactsApi.DuoArtifactVanillaDeck,
+				[ArtifactPool.Common],
+				helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile($"Sprites/artifact_icons/duos/wax_wings.png")).Sprite
+			);
+			ModEntry.Instance.DuoArtifactsApi.RegisterDuoArtifact(MethodBase.GetCurrentMethod()!.DeclaringType!, [ModEntry.Instance.EddieDeck, SogginsAPI.SogginsVanillaDeck]);
+		}
+	}
+
 	public override void OnQueueEmptyDuringPlayerTurn(State state, Combat combat)
 	{
-		if (SogginsAPI != null) {
-			active = SogginsAPI.IsOversmug(state, state.ship);
-		}
+		active = SogginsAPI!.IsOversmug(state, state.ship);
 	}
 
 	public void OnCardBotchedBySmug(State state, Combat combat, Card card) {
 		if (active) {
 			Pulse();
-			combat.Queue(new List<CardAction> {
+			combat.Queue([
 				new ADelay {
 					time = 0.3
 				},
 				new AEnergy {
-					changeAmount = 3
+					changeAmount = 2
 				}
-			});
+			]);
 		}
 	}
 }
