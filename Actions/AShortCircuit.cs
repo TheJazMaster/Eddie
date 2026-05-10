2b4545a2f0e4230c0450ec34cd3ee55889a1b99e
Aljaž Perovšek <aljazpero@hotmail.com>
2026-05-10 18:28:06 +0200

    Removed hardcoded Closed Circuit interaction, code cleanup

--- Actions/AShortCircuit.cs
+++ Actions/AShortCircuit.cs
@@ -1,14 +1,10 @@
-using System;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FSPRO;
 
 namespace TheJazMaster.Eddie.Actions;
 
 public class AShortCircuit : CardAction
 {
+    public int amount = 2;
     public int startEnergy;
 
     public override void Begin(G g, State s, Combat c)
@@ -18,33 +14,18 @@ public class AShortCircuit : CardAction
             return;
         }
 
-        int count = 2;
-        int ccStatus = s.ship.Get((Status)Manifest.ClosedCircuitStatus.Id!);
-        if (ccStatus > 0) {
-            int difference = Math.Min(count, ccStatus);
-            count -= difference;
-            s.ship.Add((Status)Manifest.ClosedCircuitStatus.Id!, -difference);
-        }
-
-        List<Card> list = [];
         int num = 0;
-        for (int i = 0; i < count && i < c.hand.Count; i++)
-        {
-            list.Add(c.hand[i]);
-        }
-        foreach (Card item in list)
+        foreach (Card item in c.hand.Take(amount))
         {
             c.hand.Remove(item);
             item.waitBeforeMoving = ++num * 0.16;
             item.OnDiscard(s, c);
             c.SendCardToDiscard(s, item);
         }
-        if (list.Count > 0)
+        if (num > 0)
         {
-            Manifest.Instance.KokoroApi.SetExtensionData<double>(s.ship, "ShortCircuitOverlay", 1); 
+            ModEntry.Instance.Helper.ModData.SetModData<double>(s.ship, "ShortCircuitOverlay", 1); 
             Audio.Play(Event.CardHandling);
         }
     }
-
-    public override List<Tooltip> GetTooltips(State s) => [new TTGlossary(Manifest.DiscardLeftmostGlossary?.Head!, 2)];
 }
